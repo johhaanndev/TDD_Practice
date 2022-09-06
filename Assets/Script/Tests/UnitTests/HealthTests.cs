6afaec88fcdd3d052412850efcdba72cd3a15825
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2022-09-06 21:13:23 +0200

    Added tests for replenish method

--- Assets/Script/Tests/UnitTests/HealthTests.cs
+++ Assets/Script/Tests/UnitTests/HealthTests.cs
@@ -1,34 +1,67 @@
-using NUnit.Framework;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.TestTools;
 using UnityEngine.UI;
 
-public class HealthTests
+namespace Game.Tests.UnitTests
 {
-    [Test]
-    public void WhenReplenishHeartWith0Pieces_ImageIsFilledWith0PercentAmount()
+
+    public class HealthTests
     {
-        var image = new GameObject().AddComponent<Image>();
-        image.fillAmount = 0;
-        var heart = new Heart(image);
+        [Test]
+        public void WhenReplenishHeartWith0Pieces_IfHeartIsEmpty_ImageIsFilledWith0PercentAmount()
+        {
+            var image = new GameObject().AddComponent<Image>();
+            image.fillAmount = 0;
+            var heart = new Heart(image);
 
-        heart.Replenish(0); 
+            heart.Replenish(0);
 
-        Assert.Equals(0, image.fillAmount);
+            Assert.AreEqual(0, image.fillAmount);
+        }
+
+        [Test]
+        public void WhenReplenishHeartWith1Piece_IfHeartIsEmpty_ImageIsFilledWith25PercentAmount()
+        {
+            var image = new GameObject().AddComponent<Image>();
+            image.fillAmount = 0;
+            var heart = new Heart(image);
+
+            heart.Replenish(1);
+
+            Assert.AreEqual(0.25f, image.fillAmount);
+        }
+
+        [Test]
+        public void WhenReplenishHeartWith1Piece_IfHeartIsAlready25PercentFilled_ImageIsFilledWith50PercentAmount()
+        {
+            var image = new GameObject().AddComponent<Image>();
+            image.fillAmount = 0.25f;
+            var heart = new Heart(image);
+
+            heart.Replenish(1);
+
+            Assert.AreEqual(0.5f, image.fillAmount);
+        }
     }
-}
 
-public class Heart
-{
-    private Image image;
-
-    public Heart(Image image)
+    public class Heart
     {
-        this.image = image;
+        private const float FillPerHeartPiece = 0.25f;
+        private Image _image;
+
+        public Heart(Image image)
+        {
+            _image = image;
+        }
+
+        public void Replenish(int numberOfHeartPieces)
+        {
+            _image.fillAmount += numberOfHeartPieces * FillPerHeartPiece;
+        }
     }
 
-    public void Replenish(int numberOfHeartPieces)
-    {
-        throw new NotImplementedException();
-    }
 }
