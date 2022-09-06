fb7036c4dd84e21c4ab59373a945af053715bebf
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2022-09-06 20:56:08 +0200

    created test to fill image with 0 percent

--- Assets/Script/Tests/HealthTests.cs
+++ Assets/Script/Tests/HealthTests.cs
@@ -1,16 +1,34 @@
 using NUnit.Framework;
+using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthTests
 {
     [Test]
-    public void TestReplenish()
+    public void WhenReplenishHeartWith0Pieces_ImageIsFilledWith0PercentAmount()
     {
-        var image = new Image();
+        var image = new GameObject().AddComponent<Image>();
+        image.fillAmount = 0;
         var heart = new Heart(image);
 
         heart.Replenish(0); 
 
         Assert.Equals(0, image.fillAmount);
+    }
+}
+
+public class Heart
+{
+    private Image image;
+
+    public Heart(Image image)
+    {
+        this.image = image;
+    }
+
+    public void Replenish(int numberOfHeartPieces)
+    {
+        throw new NotImplementedException();
     }
 }
