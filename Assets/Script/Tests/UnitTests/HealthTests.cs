01339cbf625a27551ff2f27e9569312af3e4a82c
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2022-09-06 21:23:31 +0200

    Extracted heart class and created another class for Deplete method

--- Assets/Script/Tests/UnitTests/HealthTests.cs
+++ Assets/Script/Tests/UnitTests/HealthTests.cs
@@ -8,65 +8,56 @@ using UnityEngine.UI;
 
 namespace Game.Tests.UnitTests
 {
-
     public class HealthTests
     {
-        private Image _image;
-        private Heart _heart;
-
-        [SetUp]
-        public void BeforeEveryTest()
+        public class ReplenishMethod
         {
-            _image = new GameObject().AddComponent<Image>();
-            _heart = new Heart(_image);
+
+            private Image _image;
+            private Heart _heart;
+
+            [SetUp]
+            public void BeforeEveryTest()
+            {
+                _image = new GameObject().AddComponent<Image>();
+                _heart = new Heart(_image);
+            }
+
+            [Test]
+            public void WhenReplenishHeartWith0Pieces_IfHeartIsEmpty_ImageIsFilledWith0PercentAmount()
+            {
+                _image.fillAmount = 0;
+
+                _heart.Replenish(0);
+
+                Assert.AreEqual(0, _image.fillAmount);
+            }
+
+            [Test]
+            public void WhenReplenishHeartWith1Piece_IfHeartIsEmpty_ImageIsFilledWith25PercentAmount()
+            {
+                _image.fillAmount = 0;
+                _heart = new Heart(_image);
+
+                _heart.Replenish(1);
+
+                Assert.AreEqual(0.25f, _image.fillAmount);
+            }
+
+            [Test]
+            public void WhenReplenishHeartWith1Piece_IfHeartIsAlready25PercentFilled_ImageIsFilledWith50PercentAmount()
+            {
+                _image.fillAmount = 0.25f;
+                var _heart = new Heart(_image);
+
+                _heart.Replenish(1);
+
+                Assert.AreEqual(0.5f, _image.fillAmount);
+            }
         }
 
-        [Test]
-        public void WhenReplenishHeartWith0Pieces_IfHeartIsEmpty_ImageIsFilledWith0PercentAmount()
+        public class DepleteMethod
         {
-            _image.fillAmount = 0;
-
-            _heart.Replenish(0);
-
-            Assert.AreEqual(0, _image.fillAmount);
-        }
-
-        [Test]
-        public void WhenReplenishHeartWith1Piece_IfHeartIsEmpty_ImageIsFilledWith25PercentAmount()
-        {
-            _image.fillAmount = 0;
-            _heart = new Heart(_image);
-
-            _heart.Replenish(1);
-
-            Assert.AreEqual(0.25f, _image.fillAmount);
-        }
-
-        [Test]
-        public void WhenReplenishHeartWith1Piece_IfHeartIsAlready25PercentFilled_ImageIsFilledWith50PercentAmount()
-        {
-            _image.fillAmount = 0.25f;
-            _heart = new Heart(_image);
-
-            _heart.Replenish(1);
-
-            Assert.AreEqual(0.5f, _image.fillAmount);
-        }
-    }
-
-    public class Heart
-    {
-        private const float FillPerHeartPiece = 0.25f;
-        private Image _image;
-
-        public Heart(Image image)
-        {
-            _image = image;
-        }
-
-        public void Replenish(int numberOfHeartPieces)
-        {
-            _image.fillAmount += numberOfHeartPieces * FillPerHeartPiece;
         }
     }
 
---
+++ Assets/Script/Tests/UnitTests/Heart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.UI;
+
+namespace Game.Tests.UnitTests
+{
+    public class Heart
+    {
+        private const float FillPerHeartPiece = 0.25f;
+        private Image _image;
+
+        public Heart(Image image)
+        {
+            _image = image;
+        }
+
+        public void Replenish(int numberOfHeartPieces)
+        {
+            _image.fillAmount += numberOfHeartPieces * FillPerHeartPiece;
+        }
+    }
+
+}
